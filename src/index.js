f79045db3e9a8986e6e8d7b89a5675f88f0398b9
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-02-28 20:35:28 -0500

    Added in the initialState to reducers, made mapToProps accessible and passed into app, created the actions

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { createStore } from 'redux';
-// import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
+
+import { reducer } from './reducers';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-// const store = createStore();
+const store = createStore(reducer);
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
-	// <Provider store={store}>
-	<App />,
-	// </Provider>,
+	<Provider store={store}>
+		<App />
+	</Provider>,
 	rootElement
 );
